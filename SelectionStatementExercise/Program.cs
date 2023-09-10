1ed70023010ce49657fcbd2897bdf29100264675
Morgan Kaufman <Morgankaufman13@yahoo.com>
2023-09-10 14:44:42 -0400

    Selection-Statements Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favNumber = 43;
+            var guessedCorrectly = false;
+
+            while (!guessedCorrectly)
+            {
+
+                Console.WriteLine("Guess a number");
+
+                var userInput = Console.ReadLine();
+
+
+                if (int.TryParse(userInput, out int userGuess))
+                {
+
+                    if (userGuess == favNumber)
+                    {
+                        Console.WriteLine("awesome! thats right!");
+                        guessedCorrectly = true;
+
+                    }
+
+                    else if (userGuess > favNumber)
+                    {
+                        Console.WriteLine("Too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too low");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("invalid number guess again");
+                }
+            }
         }
     }
 }
+
